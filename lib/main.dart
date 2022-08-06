39670d91a9e0dd303fabf0587a600f29e6c07f33
pine <yamakiyuto0830@gmail.com>
2022-08-07 00:46:44 +0900

    forth commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:url_launcher/url_launcher.dart';
 import 'bookpage.dart';
 
 void main() => runApp(const MyApp());
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
